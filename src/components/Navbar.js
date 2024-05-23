c6251671d8d5d47ca771059006b73d45b144fc07
Saketh <sandragirisaki2004@gmail.com>
2024-05-24 00:01:17 +0530

    Your commit message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Sakeths2004/SakethPortfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
